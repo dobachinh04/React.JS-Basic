e3eef93b2ee249ffcc5516247070f75e94431cb7
DoBaChinh <dobachinh04@gmail.com>
2024-03-24 18:26:27 +0700

    Start Project - Meme Generator

---
+++ meme-generator/src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="Container">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
